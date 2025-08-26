0a4c49f55ace7532b07da30e89f4983bbfbbbfd6
Brian Li <brianwli617@gmail.com>
2025-08-25 21:56:51 -0400

    updated script

--- indexfasta.py
+++ indexfasta.py
@@ -1,19 +1,14 @@
-with open("random_1mbp.fasta", 'r') as f:
+import sys
+file_path = sys.argv[1]
+start = int(sys.argv[2])
+end = int(sys.argv[3])
+
+with open(file_path, 'r') as f:
     string = ""
     for line in f:
         if line.startswith(">"):
-            print("header")
             continue
         else:
             string = string + line.strip("\n")
 
-    print(string[886503:886530])
-    print(string[412273:412302])
-
-    print(string[511502:511521])
-    print(string[511501:511530])
-
-    print(string[568128:568158])
-    print(string[568159:568174])
-
-    print(string[568128:568174])
+    print(string[start:end])
